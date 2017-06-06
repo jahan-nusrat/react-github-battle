91a24400bf37319b91eced45ede64ae8f9b784e4
Golam Rabbani <knight3rrantt@gmail.com>
2017-06-06 22:29:18 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Popular from './Popular';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Popular/>
+      </div>
+    );
+  }
+}
+
+export default App;
